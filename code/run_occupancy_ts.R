13f96d5aaf4714db7b55681e13da7a668daf1622
aterui <hanabi0111@gmail.com>
2022-05-10 13:37:30 -0400

    Squashed commit of the following:
    commit 513763f27d359b7cf0a4b62d955fe0d531bffee9
    Author: aterui <hanabi0111@gmail.com>
    Date:   Tue May 10 13:36:37 2022 -0400
    
        Update model_occupancy_ts.R
    
    commit 78d1f28b6dc800aef9b29fb86d4415bc81bf34cc
    Author: aterui <hanabi0111@gmail.com>
    Date:   Tue May 10 13:21:59 2022 -0400
    
        edit: model
    
    commit 0d7f2584943446df6d416fd91c52d420a3ce3ee6
    Author: cfwahl <89475052+cfwahl@users.noreply.github.com>
    Date:   Mon May 9 10:06:57 2022 -0400
    
        removed charlie-edit folder from branch
    
    commit 4ee30bc71ba46a489f16408ebbb67bce3da759b3
    Author: cfwahl <89475052+cfwahl@users.noreply.github.com>
    Date:   Mon May 9 10:05:14 2022 -0400
    
        moved files from "charlie-edit" folder to to appropriate folders within the branch
    
    commit e0eeb168e76f15ec37bc2ce936816ed9f450cb20
    Author: cfwahl <89475052+cfwahl@users.noreply.github.com>
    Date:   Thu Apr 28 05:12:33 2022 -0400
    
        code used to re-run gis and bayes analyses
    
    commit fc7e94b27b87702eb563b900398ea65a9b0d6952
    Author: cfwahl <89475052+cfwahl@users.noreply.github.com>
    Date:   Wed Apr 27 20:30:41 2022 -0400
    
        create working branch for myself
    
    commit a3e34b41964e8474f1e4398065a3f32d0546f031
    Merge: 64a64aa 3efef3f
    Author: cfwahl <89475052+cfwahl@users.noreply.github.com>
    Date:   Wed Apr 27 10:39:53 2022 -0400
    
        Merge branch 'terui-edit' into charlie-edit
    
    commit 64a64aa6d197d6ed3e8adf6ae70658cd29634c00
    Author: cfwahl <89475052+cfwahl@users.noreply.github.com>
    Date:   Thu Apr 21 14:56:14 2022 -0400
    
        files

--- code/run_occupancy_ts.R
+++ code/run_occupancy_ts.R
@@ -16,7 +16,7 @@ pacman::p_load(runjags,
 
 load("data_fmt/distance_matrix.RData")
 load("data_fmt/data_mn_fmt.RData")
-df_landuse <- sf::st_read(dsn = "data_fmt/vector/espg3722_watersheds_landuse.gpkg") %>% 
+df_landuse <- sf::st_read(dsn = "data_fmt/vector/espg3722_watersheds_landuse2.gpkg") %>% 
   as_tibble()
 
 data <- df_mn %>% 
@@ -69,7 +69,7 @@ para <- c("alpha",
           "sd_r")
 
 ## model file ####
-m <- read.jagsfile("code/model_occupancy_ts8.R")
+m <- read.jagsfile("code/model_occupancy_ts.R")
 
 ## mcmc setup ####
 n_ad <- 100 
