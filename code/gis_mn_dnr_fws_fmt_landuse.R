ada84ded5d7f4ab7a1e70e7af5adb438ead7ed83
cfwahl <89475052+cfwahl@users.noreply.github.com>
2022-07-09 13:26:53 -0400

    added the FWS stream data and updated all files

---
+++ code/gis_mn_dnr_fws_fmt_landuse.R
@@ -0,0 +1,114 @@
+##### calculate land use in watersheds ------------------------------------
+
+# this section of code will calculate percent land use for agriculture, 
+# forest, urban, grassland, wetlands, plus climate variables 
+
+
+# setup -------------------------------------------------------
+
+# clean objects
+rm(list = ls())
+
+pacman::p_load(raster,
+               tidyverse,
+               sf,
+               exactextractr)  
+
+# read data ---------------------------------------------------------------
+
+## watershed polygon ##
+
+# transform to projected crs for extraction - 
+# extraction needs to be performed with projected crs
+wgs84_sf_wsd <- st_read(dsn = "data_fmt/vector/epsg4326_mn_fmt_watersheds_5km2.shp")
+utm_sf_wsd <- wgs84_sf_wsd %>% 
+  st_transform(crs = 3722)
+
+
+## land use ##
+
+## transform to projected crs for extraction - extraction needs to be performed with projected crs
+## "discrete" raster must be reprojected with "ngb" (nearest neigbor)
+wgs84_rs_lu <- raster("data_fmt/raster/epsg4326_nlcd_2019_study_area.tif") %>% 
+  crop(extent(wgs84_sf_wsd)) # crop by extent of watershed layer
+
+# raster extraction -------------------------------------------------------
+
+utm_rs_lu <- projectRaster(from = wgs84_rs_lu,
+                           crs = st_crs(3722)$proj4string,
+                           method = 'ngb',
+                           res = 30)
+
+## create binary raster for each land use type (1,0 entry for each cell)
+## land use code must be adapted
+utm_rs_urban <- calc(utm_rs_lu,
+                     fun = function(x) ifelse(dplyr::between(x, 21, 24), 1, 0))
+
+utm_rs_forest <- calc(utm_rs_lu,
+                      fun = function(x) ifelse(dplyr::between(x, 41, 52), 1, 0))
+
+utm_rs_agri <- calc(utm_rs_lu,
+                    fun = function(x) ifelse(dplyr::between(x, 81, 82), 1, 0))
+
+utm_rs_grass <- calc(utm_rs_lu,
+                     fun = function(x) ifelse(x == 71, 1, 0))
+
+utm_rs_wet <- calc(utm_rs_lu,
+                   fun = function(x) ifelse(dplyr::between(x, 90, 95), 1, 0))
+
+
+utm_rs_fua <- raster::stack(utm_rs_forest,
+                            utm_rs_urban,
+                            utm_rs_agri, 
+                            utm_rs_grass,
+                            utm_rs_wet) # combine 5 layers into one "stack"
+
+names(utm_rs_fua) <- c("forest", "urban", "agri", "grass", "wetland")
+
+
+## climate ####
+
+## example climate data (annual mean temperature) - CHELSA ver 2.1 (30 arcsecond ~ 1km)
+## "continuous" raster must be reprojected with "bilinear"
+## CHELSA layer from "https://chelsa-climate.org/downloads/"
+wgs84_rs_temp <- raster("data_fmt/raster/chelsa_clip.tif") %>% 
+  crop(extent(wgs84_sf_wsd)) # crop by extent of watershed layer
+
+utm_rs_temp <- projectRaster(from = wgs84_rs_temp,
+                             crs = st_crs(3722)$proj4string,
+                             method = 'bilinear',
+                             res = 1000)
+
+# extraction by polygon ---------------------------------------------------
+
+## land use
+df_lu <- exact_extract(utm_rs_fua, # raster layer for extraction
+                       utm_sf_wsd, # masking layer
+                       "mean",
+                       append_cols = "siteid") %>% # take mean for each polygon
+  as_tibble() %>% 
+  rename(frac_forest = mean.forest,
+         frac_urban = mean.urban,
+         frac_agri = mean.agri,
+         frac_grass = mean.grass,
+         frac_wetland = mean.wetland)
+
+## climate
+df_temp <- exact_extract(utm_rs_temp, # raster layer for extraction
+                         utm_sf_wsd, # masking layer
+                         "mean",
+                         append_cols = "siteid") %>%   # take mean for each polygon
+  as_tibble() %>% 
+  rename(temp = mean)
+
+
+# merge data --------------------------------------------------------------
+
+utm_sf_wsd <- utm_sf_wsd %>% 
+  left_join(df_lu, by = "siteid") %>% 
+  left_join(df_temp, by = "siteid") %>% 
+  mutate(area = units::set_units(st_area(.), "km^2")) # add watershed area
+
+st_write(utm_sf_wsd,
+         dsn = "data_fmt/vector/espg3722_watersheds_landuse_5km2.gpkg",
+         append = FALSE)
---
+++ code/gis_mn_dnr_fws_fmt_whitebox_delineation.R
@@ -0,0 +1,188 @@
+
+# watershed delineation using whitebox package
+
+# setup -------------------------------------------------------------------
+
+pacman::p_load(sf,
+               raster,
+               stars,
+               whitebox,
+               mapview,
+               tidyverse, sp)
+
+rm(list = ls())
+
+
+# DEM setup ---------------------------------------------------------------
+# - Read flow direction raster
+wgs84_dir <- raster("data_fmt/raster/epsg4326_n40w100_dir_clipped.tif")
+
+# - convert arc format to d8 format
+source("code/function_arc2d8.R")
+wgs84_dir_d8 <- arc2d8(wgs84_dir)
+
+writeRaster(wgs84_dir_d8,
+            "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif",
+            options = c('TFW = YES'),
+            overwrite = TRUE)
+
+
+# gis ---------------------------------------------------------------------
+
+# ## Extract stream network
+# - the function wbt_extract_streams was used to extract stream network
+# - threshold is 1 = 1 km^2
+wbt_extract_streams(flow_accum = "data_fmt/raster/epsg4326_n40w100_upa_clipped.tif",
+                    output = "data_fmt/raster/epsg4326_mn_fmt_stream_5km2.tif",
+                    threshold = 5) # Based on MERIT hydro, 1 indicates 1 km^2
+
+# ## Snap pour point
+# - the function wbt_jenson_snap_pour_points rather than wbt_snap_pour_points
+# - This function snap the spoint to stream network and it wored better than snapping into catchment area
+wbt_jenson_snap_pour_points(pour_pts = "data_fmt/vector/epsg4326_mn_dnr_fws_fmt_sites_snap.shp", # GPKG file is not accepted. Used SHP file.
+                            streams = "data_fmt/raster/epsg4326_mn_fmt_stream_5km2.tif",
+                            output = "data_fmt/vector/epsg4326_mn_dnr_fws_fmt_sites_snap2.shp",
+                            snap_dist = 1)
+
+# Assigns a unique identifier to each link in a stream network
+wbt_stream_link_identifier(d8_pntr = "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif",
+                           streams = "data_fmt/raster/epsg4326_mn_fmt_stream_5km2.tif",
+                           output = "data_fmt/raster/epsg4326_mn_fmt_stream_id_5km2.tif")
+
+wbt_stream_slope_continuous(d8_pntr = "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif", 
+                            streams = "data_fmt/raster/epsg4326_mn_fmt_stream_id_5km2.tif", 
+                            dem = "data_fmt/raster/epsg4326_n40w100_elv_clipped.tif",
+                            output = "data_fmt/raster/epsg4326_mn_fmt_stream_slope_cont_5km2.tif")
+
+wbt_zonal_statistics(input = "data_fmt/raster/epsg4326_mn_fmt_stream_slope_cont_5km2.tif",
+                     features = "data_fmt/raster/epsg4326_mn_fmt_stream_id_5km2.tif",
+                     output = "data_fmt/raster/epsg4326_mn_fmt_stream_link_slope_5km2.tif",
+                     stat = "mean")
+
+# Converts link slope raster into a vector file.
+wbt_raster_streams_to_vector(streams = "data_fmt/raster/epsg4326_mn_fmt_stream_link_slope_5km2.tif", 
+                             d8_pntr = "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif",
+                             output = "data_fmt/vector/epsg4326_mn_str_slope_5km2.shp")
+
+# Estimates the average slope of each link (or tributary) in a stream network
+#wbt_stream_link_slope(d8_pntr = "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif",
+#                      linkid = "data_fmt/raster/epsg4326_mn_fmt_stream_id_5km2.tif",
+#                      dem = "data_fmt/raster/epsg4326_n40w100_elv_clipped.tif", 
+#                      output = "data_fmt/raster/epsg4326_mn_fmt_stream_link_slope_5km2.tif")
+#
+#wbt_strahler_stream_order(d8_pntr = "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif",
+#                          streams = "data_fmt/raster/epsg4326_mn_fmt_stream_id_5km2.tif",
+#                          output = "data_fmt/raster/epsg4326_mn_fmt_stream_order_5km2.tif")
+#
+# Converts stream order raster into a vector file.
+#wbt_raster_streams_to_vector(streams = "data_fmt/raster/epsg4326_mn_fmt_stream_order_5km2.tif", 
+#                             d8_pntr = "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif",
+#                             output = "data_fmt/vector/epsg4326_mn_stream_order_5km2.shp")
+
+
+# associate line id -------------------------------------------------------
+
+point <- st_read(dsn = "data_fmt/vector",
+                 layer = "epsg4326_mn_dnr_fws_fmt_sites_snap2")
+
+line <- st_read(dsn = "data_fmt/vector",
+                layer = "epsg4326_mn_str_slope_5km2") %>% 
+  st_set_crs(4326)
+
+
+site.info <- point %>% 
+  mutate(line_id = st_nearest_feature(., line)) %>% 
+  left_join(as_tibble(line),
+            by = c("line_id" = "FID")) %>%
+  select(occurrence:geometry.x) # remove line geometry
+
+
+# export ------------------------------------------------------------------
+
+# site info, all individual sites 
+st_write(site.info,
+         dsn = "data_fmt/vector/epsg4326_mn_dnr_fws_fmt_site_link.shp",
+         append = FALSE)
+
+
+# aggregate sites with line_id --------------------------------------------
+
+line_id <- site.info %>%
+  group_by(line_id)  %>%
+  summarize(occurrence = sum(occurrence),
+            slope = mean(STRM_VAL)) %>%
+  mutate(occurrence = replace(occurrence, occurrence > 0, 1)) 
+  
+# creates one point that is the centroid on the points along a line segment
+line_centroid <- st_centroid(line_id) 
+snapped_points <- spNetwork::snapPointsToLines2(line_centroid, # snap centroid to stream
+                                     line,
+                                     "line_id") %>% 
+  mutate(siteid = row_number())
+
+# export ------------------------------------------------------------------
+
+# line id, sites within a stream segment grouped
+st_write(line_id,
+         dsn = "data_fmt/vector/epsg4326_mn_dnr_fws_line_id.gpkg",
+         append = FALSE)
+
+# site info, one point representing all sites along a stream segment
+st_write(snapped_points,
+         dsn = "data_fmt/vector/epsg4326_mn_dnr_fws_line_centroid.shp",
+         append = FALSE)
+
+
+
+# Unnested watershed delineation ------------------------------------------
+
+# - This function generate multiple rasterfiles, thus I have made different folders to save the file
+wbt_unnest_basins(d8_pntr = "data_fmt/raster/epsg4326_mn_fmt_flow_dir_clip_reclass.tif",
+                  pour_pts= "data_fmt/vector/epsg4326_mn_dnr_fws_line_centroid.shp", # GPKG file is not accepted. Used SHP file.
+                  output = "data_fmt/wsraster/unnestedws.tif")
+
+# ## Read result of delineated watershed raster files
+# list.files() calls file names in the folder
+# lapply() applies function to each element
+wgs84_list_raster <- list.files("data_fmt/wsraster",
+                                full.names = TRUE) %>%
+  lapply(FUN = raster)
+
+# ## Convert rater to shape
+wgs84_sf_ws_polygon <- lapply(wgs84_list_raster,
+                              function(x) {
+                                st_as_stars(x) %>% 
+                                  st_as_sf(merge = TRUE,
+                                           as_points = FALSE) %>% 
+                                  rename(siteid = starts_with("unnested"))
+                              }) %>% 
+  bind_rows() %>% 
+  st_transform(crs = 3722) %>% 
+  mutate(area = units::set_units(st_area(.), "km^2")) %>% 
+  filter(area > units::set_units(5, "km^2")) %>% 
+  st_transform(crs = 4326)
+
+
+# export ------------------------------------------------------------------
+
+# watersheds
+st_write(wgs84_sf_ws_polygon,
+         dsn = "data_fmt/vector/epsg4326_mn_fmt_watersheds_5km2.shp",
+         append = FALSE)
+
+
+# join --------------------------------------------------------------------
+
+# add line_id from site point file to watershed polygon attribute table
+wbt_join_tables(input1 = "data_fmt/vector/epsg4326_mn_fmt_watersheds_5km2.shp",
+                pkey = "siteid",
+                input2 = "data_fmt/vector/epsg4326_mn_dnr_fws_line_centroid.shp",
+                fkey = "siteid",
+                import_field = "line_id")
+
+# add line_id from site point file to watershed polygon attribute table
+wbt_join_tables(input1 = "data_fmt/vector/epsg4326_mn_fmt_watersheds_5km2.shp",
+                pkey = "siteid",
+                input2 = "data_fmt/vector/epsg4326_mn_dnr_fws_line_centroid.shp",
+                fkey = "siteid",
+                import_field = "slope")
