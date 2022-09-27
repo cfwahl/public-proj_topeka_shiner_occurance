341fac0c74428927605fb998dccd421ac13680a4
cfwahl <89475052+cfwahl@users.noreply.github.com>
2022-09-27 11:45:05 -0400

    changed files names, updated script, cleaned up code

---
+++ code/figure_occurrence_regressions.R
@@ -0,0 +1,154 @@
+# setup -------------------------------------------------------------------
+
+# clean objects
+rm(list = ls())
+
+# Load packages
+pacman::p_load(runjags,
+               tidyverse,
+               MCMCvis,
+               mcmcOutput,
+               foreach)
+
+## read in mcmc summary output
+mcmc_summary_up_full <- readRDS(here::here("output/mcmc_summary_up_full.rds"))
+mcmc_summary_up_full <- readRDS("output/mcmc_summary_up_full.rds")
+
+# common data -------------------------------------------------------------
+
+## extract estimated connectivity values
+df_s <- mcmc_summary_up_full %>% 
+  as_tibble() %>% 
+  filter(str_detect(param, "s_hat\\[.{1,}\\]")) %>% # remove alpha/beta, only want s_hat
+  mutate(siteid = as.numeric(str_extract(param, "\\d{1,}"))) %>% 
+  select(siteid,
+         s = `50%`) # select only siteid and connectivity value, or s
+
+## data used for analysis
+df_actual_occurrence <- sf::st_read(dsn = "data_fmt/vector/espg3722_watersheds_landuse_dummy_2_5km2.gpkg") %>% 
+  as_tibble() %>%
+  filter(!is.na(occrrnc)) %>%  # filter out NA data for occurrence 
+  left_join(df_s,
+            by = "siteid")
+
+## regression slopes  
+df_beta <- mcmc_summary_up_full %>% 
+  select(param,
+         median = `50%`) %>% # select param and median columns 
+  filter(str_detect(string = param,
+                    pattern = "mu_r|beta\\[.{1,}\\]")) %>% # keep only beta and mu values
+  mutate(param = factor(param,
+                        levels = c("mu_r",
+                                   paste0("beta[", 1:5, "]")))) %>% 
+  arrange(param) # move mu_r to the top
+
+df_prediction <- df_actual_occurrence %>% 
+  summarize(across(.cols = c(starts_with("frc"), frac_gr, tmp_ssn, area, prcp_wt, s),
+                   .fns = function(x) seq(from = min(x, na.rm = T),
+                                          to = max(x, na.rm = T),
+                                          length = 100))) %>% # select these variables
+  mutate(mean_agri = mean(df_actual_occurrence$frac_gr), # add columns with mean values for these variables 
+         mean_temp = mean(df_actual_occurrence$tmp_ssn),
+         mean_area = mean(df_actual_occurrence$area),
+         mean_precip = mean(df_actual_occurrence$prcp_wt),
+         mean_s = mean(df_actual_occurrence$s))
+
+x_name <- df_prediction %>% 
+  select(!starts_with("mean")) %>% 
+  colnames() # select the 9 column names that do not start with "mean"
+
+
+# prediction --------------------------------------------------------------
+
+## extract mean values for each predictor
+df_y <- foreach(i = 1:length(x_name),
+                .combine = bind_rows) %do% {
+                  
+                  X <- df_prediction %>% 
+                    select(starts_with("mean")) %>% # extract mean value for all rows combined
+                    data.matrix()
+                  
+                  ## replace the column of the predictor of interest
+                  x_focus <- df_prediction %>% 
+                    select(x_name[i]) %>% 
+                    pull()
+                  
+                  X[, i] <- x_focus # THIS LINE DOES NOT WORK FOR ME, SCRIPT OUT OF BOUNDS
+                  M <- model.matrix(~ X)
+                  
+                  y <- c(boot::inv.logit(M %*% pull(df_beta, median)))
+                  
+                  df_y0 <- tibble(y = y,
+                                  x = x_focus,
+                                  focus = colnames(df_prediction)[i])
+                  
+                  return(df_y0)    
+                }
+
+## reformat to long-form
+df_data_l <- df_actual_occurrence %>% 
+  pivot_longer(cols = x_name,
+               values_to = "x",
+               names_to = "focus")
+
+
+# regression plot ---------------------------------------------------------
+
+df_y %>% 
+  ggplot(aes(x = x,
+             y = y)) +
+  geom_line() +
+  geom_point(data = df_data_l,
+             aes(y = occrrnc),
+             alpha = 0.2) +
+  facet_wrap(facets = ~ focus,
+             scales = "free_x",
+             strip.position = "bottom",
+             labeller = labeller(focus = c(`area` = "Watershed area",
+                                           `frac_gr` = "Prop. of agriculture",
+                                           `prcp_wt` = "Precipitation",
+                                           `s` = "Connectivity",
+                                           `tmp_ssn` = "Temperature"))) +
+  labs(y = "Occurrence prob.") +
+  theme_bw() +
+  theme(strip.background = element_blank(),
+        strip.placement = "outside",
+        axis.title.x = element_blank())
+  
+
+
+# caterpillar bar plot -----------------------------------------------------
+
+## read in jags output
+post <- readRDS("output/post_summary_up_full.rds")
+
+# connectivity, alpha only
+MCMCplot(post$mcmc, 
+         params = 'alpha', 
+         horiz = FALSE,
+         rank = TRUE,
+         ref_ovl = FALSE,
+         xlab = 'My x-axis label', 
+         main = 'MCMCvis plot', 
+)
+
+# alpha and beta 
+#MCMCplot(post$mcmc, 
+#         params = c('alpha', 'beta'),
+#         ci = c(50, 95),
+#         HPD = TRUE,
+#         ref_ovl = TRUE,
+#         labels = c('upstream conn.', 'downstream conn.', '% agriculture', 
+#                    'temp', 'drainage area', 'precip', 
+#                    'tot. connectivity'))
+
+# alpha and b 
+MCMCplot(post$mcmc, 
+         params = c('alpha', 'b'),
+         ci = c(50, 95),
+         HPD = TRUE,
+         ref_ovl = TRUE,
+         labels = c('upstream conn.', 'downstream conn.', '% agriculture', 
+                    'temp', 'drainage area', 'precip', 
+                    'tot. connectivity'))
+
---
+++ code/glmm_test_code.R
@@ -0,0 +1,43 @@
+# setup --------------------------------------------------------------------
+
+# clean objects
+rm(list = ls())
+
+# Load packages
+pacman::p_load(tidyverse,
+               glmm, 
+               lme4, 
+               nlme, 
+               arm)
+
+
+# data --------------------------------------------------------------------
+
+# read oxbow connectivity 
+oxbow_conn <- readRDS("data_fmt/oxbow_connectivity.rds")
+
+# read oxbow local water quality
+oxbow_wq <- readRDS("data_fmt/data_mn_fws_fmt_oxbows_2.rds") 
+
+### glmm (lme4) connectivity
+mod_glmer_conn<-glmer(occurrence ~ connectivity  +(1|line_id),
+                  data = oxbow_conn,
+                  family="binomial")
+summary(mod_glmer_conn)
+
+# other working method
+glmm <- glmm(occurrence ~ connectivity, 
+             random = siteid, 
+             family.glmm = bernoulli.glmm, varcomps.names = c(siteid),
+             m = 10^4, debug = TRUE, data = oxbow_info)
+
+
+### glmm water quality
+mod_glmer_wq<-glmer(occurrence ~  temp + dopercent + do_mgl + turb + ph + (1|year),
+                  data = oxbow_conn,
+                  family="binomial")
+summary(mod_glmer_wq)
+
+
+
+
