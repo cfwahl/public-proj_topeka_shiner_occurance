707839cf0327981225b97e07d70c8aa01cb7c337
cfwahl <89475052+cfwahl@users.noreply.github.com>
2022-10-18 12:24:00 -0400

    updated codes, shapefiles, and make output plots

--- code/gis_network_centrailty.R
+++ code/gis_network_centrailty.R
@@ -15,10 +15,30 @@ pacman::p_load(igraph,
 # data --------------------------------------------------------------------
 
 ## stream polyline
-sf_line <- list.files("data_fmt/vector",
-                      pattern = "connectivity_dummy_5km2.shp",
-                      full.names = T) %>% 
-  st_read()
+sf_line2 <- sf::st_read(dsn = "data_fmt/vector/epsg3722_stream_connectivity.shp") 
+
+
+#sf_line <- list.files("data_fmt/vector",
+#                      pattern = "connectivity_dummy_5km2.shp",
+#                      full.names = T) %>% 
+#  st_read()
+
+## whole data frame of real occurrence + dummy data
+#df_ws <- sf::st_read(dsn = "data_fmt/vector/espg3722_watersheds_landuse_dummy_5km2_fixed_geom.gpkg") 
+
+
+# create sub-watersheds -----------------------------------------------------
+
+ws1 <- subset(sf_line2, watershed=="1")
+ws2 <- subset(sf_line2, watershed=="2") 
+ws3 <- subset(sf_line2, watershed=="3") 
+ws4 <- subset(sf_line2, watershed=="4") 
+ws5 <- subset(sf_line2, watershed=="5") 
+ws6 <- subset(sf_line2, watershed=="6") 
+ws7 <- subset(sf_line2, watershed=="7") 
+
+# insestion of watershed polygon with stream segments
+#ws2_int <- st_intersection(sf_line, ws3) 
 
 # network centrality test ---------------------------------------------------------
 
@@ -26,18 +46,25 @@ sf_line <- list.files("data_fmt/vector",
 ### st_touches
 
 # examine if line segments touch, true/false
-#mat_touch <- st_touches(sf_line, sparse = FALSE) 
+mat_touch <- st_touches(ws1) 
 
 # change T/F to 1/0
-#mat_touch <- 1* mat_touch
+mat_touch <- 1* mat_touch
+
+g = graph.adjlist(mat_touch)
+c = components(g)
+table(c$membership)
+ws1$groups = c$membership
+
+
 
 
 ### network with igraph
 # source - https://r-spatial.org/r/2019/09/26/spatial-networks.html
 
 # identify edges in the stream network
-edges <- sf_line %>%
-  mutate(edgeID = c(1:n())) # assign identifer
+edges <- ws6 %>%
+  mutate(edgeID = c(1:n())) # assign identifier
 
 
 # create nodes at start and end of each line segment
@@ -156,10 +183,11 @@ tm_shape(graph %>% activate(edges) %>% as_tibble() %>% st_as_sf()) +
 # specifies the number of shortest paths that pass through an edge
 graph <- graph %>%
   activate(nodes) %>%
-  mutate(degree = centrality_degree()) %>%
-  mutate(betweenness = centrality_betweenness(weights = length)) %>%
+  mutate(degree = centrality_degree()) %>% # determine centrality degree for nodes
+  mutate(eigen = centrality_eigen(weights = NULL)) %>% # eigenvector for nodes
+  mutate(betweenness = centrality_betweenness(weights = length)) %>% # determine betweenness for nodes
   activate(edges) %>%
-  mutate(betweenness = centrality_edge_betweenness(weights = length))
+  mutate(betweenness = centrality_edge_betweenness(weights = length)) # determine betweenness for edges
 
 
 # plot graph of betweenness for nodes
@@ -169,7 +197,85 @@ ggplot() +
   scale_colour_viridis_c(option = 'inferno') +
   scale_size_continuous(range = c(0,4))
 
+# plot graph of eigen and betweenness for nodes
+ggplot() +
+  geom_sf(data = graph %>% activate(edges) %>% as_tibble() %>% st_as_sf(), col = 'grey50') + 
+  geom_sf(data = graph %>% activate(nodes) %>% as_tibble() %>% st_as_sf(), aes(col = eigen, size = betweenness)) +
+  scale_colour_viridis_c(option = 'inferno') +
+  scale_size_continuous(range = c(0,4))
+
+# graph for edges
 ggplot() +
   geom_sf(data = graph %>% activate(edges) %>% as_tibble() %>% st_as_sf(), aes(col = betweenness, size = betweenness)) +
   scale_colour_viridis_c(option = 'inferno') +
   scale_size_continuous(range = c(0,4))
+
+
+
+# centrality measurements --------------------------------------------------------
+
+# eigen centrality, same as graph
+eigen_centrality(graph, 
+                 directed = FALSE, 
+                 scale = TRUE, 
+                 weights = NULL, 
+                 options = arpack_defaults )
+
+# extract only the centrality scores
+eig  <- evcent(graph)$vector
+
+# degree or number of edges connecting to a node
+degree(graph, v = V(graph), 
+       mode = c("all", "out", "in", "total"), 
+       loops = TRUE, normalized = FALSE)
+
+# produces same node scores as above
+degree <- degree(graph)
+
+# hubs, similar to eigen
+hub <- hub.score(graph)$vector
+authority <- authority.score(graph)$vector
+
+# closeness  centrality indicates how close a node is to all other nodes in the network
+closeness <- closeness(graph)
+
+# betweenness centrality measures how often a node occurs on all 
+# shortest paths between two nodes
+betweenness <- betweenness(graph)
+
+## Reach at k=2
+reach_2 <- (ego_size(graph, 2)-1)/(vcount(graph)-1)
+
+## Reach at k=3
+reach_3 <- (ego_size(graph, 3)-1)/(vcount(graph)-1)
+
+# compare different centrality scores
+centralities <- cbind(degree, 
+                      eig, 
+                      hub,
+                      authority,
+                      closeness,
+                      reach_2, 
+                      reach_3, 
+                      betweenness)
+
+# examine how correlated the different degrees are to each other
+round(cor(centralities), 2)
+
+
+
+centr_eigen(graph,
+  directed = FALSE,
+  scale = TRUE,
+  options = arpack_defaults,
+  normalized = TRUE)
+
+alpha_centrality(graph, 
+                 nodes = V(graph), 
+                 alpha = 1, 
+                 loops = FALSE, 
+                 exo = 1, 
+                 weights = NULL, 
+                 tol = 1e-07, 
+                 sparse = TRUE )
+
