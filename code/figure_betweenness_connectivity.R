85551469fa9726022bebd3f785f1a7d5b46c0c80
cfwahl <89475052+cfwahl@users.noreply.github.com>
2023-02-27 21:30:33 -0500

    changed code to examine all stream segments

--- code/figure_betweenness_connectivity.R
+++ code/figure_betweenness_connectivity.R
@@ -10,32 +10,50 @@ rm(list = ls())
 # load libraries
 source(here::here("code/library.R")) 
 
-# MINNESOTA ---------------------------------------------------------------
 # data --------------------------------------------------------------------
 
-# import stream rds
-df_mn_strm_cent <- readRDS(file = "data_fmt/data_minnesota_stream_network_centrality.rds")
+# stream network
+sf_line2 <- sf::st_read(dsn = "data_fmt/vector/epsg3722_minnesota_stream_connectivity.shp") %>%
+  dplyr::select(-c(STRM_VA)) %>% # remove slope variable
+  rename(connectivity = connectivi)
 
-# import oxbow rds
-df_mn_ox_cent <- readRDS(file = "data_fmt/data_minnesota_oxbow_network_centrality.rds")
+# network centrality betweenness ---------------------------------------------------------
 
-# visualize connectivity-betweenness ----------------------------------------------------------
+df_b <- lapply(X = 1:n_distinct(sf_line2$watershed),
+               FUN = function(x) {
+                 df_subset <- sf_line2 %>% 
+                   filter(watershed == x)
+                 
+                 b <- df_subset %>% 
+                   st_touches() %>% 
+                   graph.adjlist() %>% 
+                   betweenness(normalized = TRUE)
+                 
+                 out <- df_subset %>% 
+                   mutate(between = b) %>% 
+                   relocate(between)
+                 
+                 return(out)
+               }) %>% 
+  bind_rows() %>%
+  as.tibble()
 
-# stream connectivity x betweenness 
-ggplot(df_mn_strm_cent,
+# join --------------------------------------------------------------------
+
+sf_line_test <- sf_line2 %>%
+  left_join(df_b,
+            by = 'line_id') %>%
+  rename(connectivity = connectivity.x)
+
+# plot --------------------------------------------------------------------
+
+# plot of betweenness and connectivity
+ggplot(sf_line_test,
        aes(x = between,
-           y = connectivity))  +
+           y = connectivity)) +
   geom_smooth(method = 'glm', se = TRUE,
               method.args = list(Gamma(link = 'log')),
               color = "black",
-              fill = "grey70") + 
-  geom_point() +
+              fill = "grey70")+
+  geom_point() + 
   theme_minimal()
-
-# oxbow connectivity x betweenness
-#ggplot(df_mn_ox_cent,
-#       aes(x = between,
-#           y = connectivity))  +
-#  geom_smooth(method = 'glm', se = TRUE,
-#              method.args = list(Gamma(link = 'log'))) + 
-#  geom_point()
